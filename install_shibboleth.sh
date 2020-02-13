2b730557f71b2206642af6c2568923dbfe9403d0
anatolyguz <anatolyguz@gmail.com>
2020-02-13 15:45:09 +0200

    Create install_shibboleth.sh

---
+++ install_shibboleth.sh
@@ -0,0 +1,10 @@
+wget http://shibboleth.net/downloads/identity-provider/latest/shibboleth-identity-provider-3.4.6.tar.gz
+tar xf shibboleth-identity-provider-3.4.6.tar.gz 
+
+# edit .bash_profile with
+# export JAVA_HOME=/usr/lib/jvm/jre-1.8.0
+# PATH=$PATH:$HOME/bin:$JAVA_HOME/bin
+
+
+shibboleth-identity-provider-3.4.6/bin/install.sh
+
