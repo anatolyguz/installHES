34d377b7cb103188915a70e0bcc799cf58526bfb
anatolyguz <anatolyguz@gmail.com>
2020-02-25 19:32:56 +0200

    Update install_utilits.sh

--- install_utilits.sh
+++ install_utilits.sh
@@ -5,3 +5,36 @@ yum install telnet -y
 yum install bash-completion -y
 yum install wget -y
 . ~/.bashrc
+
+#Colour nano
+cat > ~/.nanorc << EOF
+include /usr/share/nano/java.nanorc
+include /usr/share/nano/debian.nanorc
+include /usr/share/nano/makefile.nanorc
+include /usr/share/nano/objc.nanorc
+include /usr/share/nano/mgp.nanorc
+include /usr/share/nano/ruby.nanorc
+include /usr/share/nano/asm.nanorc
+include /usr/share/nano/php.nanorc
+include /usr/share/nano/tcl.nanorc
+include /usr/share/nano/groff.nanorc
+include /usr/share/nano/tex.nanorc
+include /usr/share/nano/sh.nanorc
+include /usr/share/nano/patch.nanorc
+include /usr/share/nano/c.nanorc
+include /usr/share/nano/css.nanorc
+include /usr/share/nano/gentoo.nanorc
+include /usr/share/nano/python.nanorc
+include /usr/share/nano/xml.nanorc
+include /usr/share/nano/ocaml.nanorc
+include /usr/share/nano/cmake.nanorc
+include /usr/share/nano/html.nanorc
+include /usr/share/nano/fortran.nanorc
+include /usr/share/nano/mutt.nanorc
+include /usr/share/nano/man.nanorc
+include /usr/share/nano/pov.nanorc
+include /usr/share/nano/nanorc.nanorc
+include /usr/share/nano/perl.nanorc
+include /usr/share/nano/awk.nanorc
+EOF
+
