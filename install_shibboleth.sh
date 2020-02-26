a9645b1f18dc86c0b6b5d2f4eedf7c32d734661b
anatolyguz <anatolyguz@gmail.com>
2020-02-26 12:07:08 +0200

    Update install_shibboleth.sh

--- install_shibboleth.sh
+++ install_shibboleth.sh
@@ -1,3 +1,22 @@
+#install java 1.8
+yum install java-1.8.0 -y 
+echo "export JAVA_HOME='/usr/lib/jvm/jre-1.8.0'" >> ~/.bashrc
+source ~/.bashrc
+
+
+# tomcat 9
+#(https://www.linuxtechi.com/install-apache-tomcat9-centos7-rhel7-ubuntu16-04/)
+wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.31/src/apache-tomcat-9.0.31-src.tar.gz
+tar -zxpvf apache-tomcat-9.0.31-src.tar.gz  -C /opt/
+mv /opt/apache-tomcat-9.0.31-src/ /opt/tomcat
+mkdir  /opt/tomcat/log
+echo "export CATALINA_HOME='/opt/tomcat'" >> ~/.bashrc
+source ~/.bashrc
+chmod +x /opt/tomcat/bin/*.sh
+cd /opt/tomcat/bin
+
+
+
 wget http://shibboleth.net/downloads/identity-provider/latest/shibboleth-identity-provider-3.4.6.tar.gz
 tar xf shibboleth-identity-provider-3.4.6.tar.gz 
 
