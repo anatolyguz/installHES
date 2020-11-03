d2f2bd1f3b71fcb38cad32b7468fe9a1835b6fa2
anatolyguz <anatolyguz@gmail.com>
2020-11-03 13:26:42 +0200

    Update install_freeradius.sh

--- install_freeradius.sh
+++ install_freeradius.sh
@@ -1,6 +1,17 @@
+IPCLIENT="192.168.1.1"
+
 # Centos 7
 
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 systemctl stop firewalld
 systemctl disable firewalld
 yum install -y freeradius freeradius-utils
+
+sed -i 's/user = radiusd/user = root/' /etc/raddb/radiusd.conf 
+sed -i 's/group = radiusd/group = root/' /etc/raddb/radiusd.conf 
+
+#Enable pam
+# was # pam
+sed  -i 's/#\tpam/pam/' /etc/raddb/sites-enabled/default
+ln -s /etc/raddb/mods-available/pam /etc/raddb/mods-enabled/pam
+
