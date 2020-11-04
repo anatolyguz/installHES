22c5827dede431845ab88cf95ff23cc270d894ea
anatolyguz <anatolyguz@gmail.com>
2020-11-04 08:50:56 +0200

    Update install_freeradius.sh

--- install_freeradius.sh
+++ install_freeradius.sh
@@ -2,12 +2,20 @@ IPCLIENT="192.168.1.1"
 
 # Centos 7
 
+#disable selinux
 sudo setenforce 0
 sed -i 's/SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
-systemctl stop firewalld
-systemctl disable firewalld
+
+#disable firewall
+if service --status-all | grep -Fq "firewalld.service"; then
+        systemctl stop firewalld
+        systemctl disable firewalld
+fi
+
+#install freeradius
 yum install -y freeradius freeradius-utils
 
+#change for start from root
 sed -i 's/user = radiusd/user = root/' /etc/raddb/radiusd.conf 
 sed -i 's/group = radiusd/group = root/' /etc/raddb/radiusd.conf 
 
