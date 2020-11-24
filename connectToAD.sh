6bdee51a706f09be92a36ebcd786dad084e1ca2a
anatolyguz <anatolyguz@gmail.com>
2020-11-24 20:42:03 +0200

    Update and rename onnectToAD.sh to connectToAD.sh

--- connectToAD.sh
+++ connectToAD.sh
@@ -18,8 +18,30 @@ DNS2="192.168.1.2"
 
 ####################################################################
 # Ubuntu 18.04
+# https://computingforgeeks.com/join-ubuntu-debian-to-active-directory-ad-domain/
 ####################################################################
+
 join_Ubuntu_18_04(){
+
+
+  hostnamectl set-hostname $HOSTNAME.$DOMAINNAME 
+
+
+  # confirm
+  hostnamectl status
+
+  echo 127.0.0.1  $HOSTNAME.$DOMAINNAME $HOSTNAME | sudo tee -a  /etc/hosts
+  echo $DCIP $DC.$DOMAINNAME $DC | sudo tee -a /etc/hosts
+ 
+ 
+ 
+ 
+  systemctl stop systemd-resolved
+  systemctl disable systemd-resolved
+  unlink /etc/resolv.conf
+  echo nameserver $DNS1 | sudo tee /etc/resolv.conf
+ 
+
 
   apt install resolvconf
   systemctl enable resolvconf.service
