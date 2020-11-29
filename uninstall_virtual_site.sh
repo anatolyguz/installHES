0e44351af4edd4a6cba0ad697f8de19addc12ddd
anatolyguz <anatolyguz@gmail.com>
2020-11-29 11:47:38 +0200

    Update uninstall_virtual_site.sh

--- uninstall_virtual_site.sh
+++ uninstall_virtual_site.sh
@@ -49,70 +49,12 @@ SETCOLOR_NORMAL="echo -en \\033[0;39m"
 ########################### end settings zone
 
 
-####################################################################
-# Get System Info
-# https://github.com/coto/server-easy-install/blob/master/lib/core.sh
-####################################################################
-lowercase(){
-	echo "$1" | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwxyz/"
-}
 
-shootProfile(){
-	OS=`lowercase \`uname\``
-	KERNEL=`uname -r`
-	MACH=`uname -m`
+###############################
+# DETECT OS
+d=$(dirname $0)
+. ${d}/detectOS.sh
 
-	if [ "${OS}" == "windowsnt" ]; then
-		OS=windows
-	elif [ "${OS}" == "darwin" ]; then
-		OS=mac
-	else
-		OS=`uname`
-		if [ "${OS}" = "SunOS" ] ; then
-			OS=Solaris
-			ARCH=`uname -p`
-			OSSTR="${OS} ${REV}(${ARCH} `uname -v`)"
-		elif [ "${OS}" = "AIX" ] ; then
-			OSSTR="${OS} `oslevel` (`oslevel -r`)"
-		elif [ "${OS}" = "Linux" ] ; then
-			if [ -f /etc/redhat-release ] ; then
-				DistroBasedOn='RedHat'
-				DIST=`cat /etc/redhat-release |sed s/\ release.*//`
-				PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-				REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
-			elif [ -f /etc/SuSE-release ] ; then
-				DistroBasedOn='SuSe'
-				PSUEDONAME=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
-				REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
-			elif [ -f /etc/mandrake-release ] ; then
-				DistroBasedOn='Mandrake'
-				PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
-				REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
-			elif [ -f /etc/debian_version ] ; then
-				DistroBasedOn='Debian'
-				if [ -f /etc/lsb-release ] ; then
-			        	DIST=`cat /etc/lsb-release | grep '^DISTRIB_ID' | awk -F=  '{ print $2 }'`
-			                PSUEDONAME=`cat /etc/lsb-release | grep '^DISTRIB_CODENAME' | awk -F=  '{ print $2 }'`
-			                REV=`cat /etc/lsb-release | grep '^DISTRIB_RELEASE' | awk -F=  '{ print $2 }'`
-            			fi
-			fi
-			if [ -f /etc/UnitedLinux-release ] ; then
-				DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
-			fi
-			OS=`lowercase $OS`
-			DistroBasedOn=`lowercase $DistroBasedOn`
-		 	readonly OS
-		 	readonly DIST
-			readonly DistroBasedOn
-		 	readonly PSUEDONAME
-		 	readonly REV
-		 	readonly KERNEL
-		 	readonly MACH
-		fi
-
-	fi
-}
-shootProfile
 #echo "OS: $OS"
 #echo "DIST: $DIST"
 #echo "PSUEDONAME: $PSUEDONAME"
@@ -120,24 +62,33 @@ shootProfile
 #echo "DistroBasedOn: $DistroBasedOn"
 #echo "KERNEL: $KERNEL"
 #echo "MACH: $MACH"
+#echo "SUB_REV=$SUB_REV
 #echo "========"
-SUB_REV=${REV:0:1} 
+
+###############################
+
+
+# Remove certificates
+
+rm rm /etc/nginx/certs/$DOMAIN_NAME.*
+###############################
+
 
 
 # Remove Nginx config
 #######################################################
 
-PATH_TO_CONFIG_DOMAIN="/etc/nginx/conf.d"
+#PATH_TO_CONFIG_DOMAIN="/etc/nginx/conf.d"
 
-if [[ $DIST == "Ubuntu" ]] 
-    then  rm /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
+if [[ $DIST == "Ubuntu" ]] ; then
+    rm /etc/nginx/sites-enabled/$DOMAIN_NAME.conf
+    rm /etc/nginx/sites-available/$DOMAIN_NAME.conf
 fi
 
+if [[ $DIST == "CentOS Linux" ]] ; then
+    rm /etc/nginx/conf.d/$DOMAIN_NAME.conf
+fi
 
-rm $PATH_TO_CONFIG_DOMAIN/$DOMAIN_NAME.conf
-
-rm /etc/nginx/certs/$DOMAIN_NAME.key
-rm /etc/nginx/certs/$DOMAIN_NAME.crt 
 
 systemctl restart nginx
 
@@ -170,3 +121,5 @@ mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "DROP DATABASE $DATABASE_NAME_FOR_MYSQ
 
 #######################################################
 
+
+echo finish!
