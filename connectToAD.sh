f57314b352ac0d21b42f26c6fdef79a6d02c2968
anatolyguz <anatolyguz@gmail.com>
2020-12-08 18:17:11 +0200

    Update connectToAD.sh

--- connectToAD.sh
+++ connectToAD.sh
@@ -52,4 +52,62 @@ join_Ubuntu_18_04(){
 
 }
 
-join_Ubuntu_18_04
+
+
+
+join_CentOS_8() {
+
+  NAMEINTERFACE=$(ip route get 8.8.8.8 | awk '{ print $5; exit }')
+  echo PEERDNS="no" >> /etc/sysconfig/network-scripts/ifcfg-$NAMEINTERFACE 
+  echo "DNS1="$DNS1 >> /etc/sysconfig/network-scripts/ifcfg-$NAMEINTERFACE 
+  echo "DNS2="$DNS2 >> /etc/sysconfig/network-scripts/ifcfg-$NAMEINTERFACE 
+
+  systemctl restart  NetworkManager.service
+
+  echo 127.0.0.1  $HOSTNAME.$DOMAINNAME $HOSTNAME >> /etc/hosts
+  echo $DCIP $DC.$DOMAINNAME $DC >> /etc/hosts 
+  #echo $DCIP $DOMAINNAME >> /etc/hosts 
+
+  #/etc/sysconfig/network-scripts/ifcfg-ens192 
+
+  # For testing  
+  realm discover $DOMAINNAME
+
+  #dnf -y install sssd realmd adcli
+  dnf -y install oddjob oddjob-mkhomedir sssd adcli
+  realm join $DOMAINNAME
+
+  #for test 
+  realm list
+  # for test 
+  id administrator@$DOMAINNAME
+
+}
+
+
+###############################
+# DETECT OS
+d=$(dirname $0)
+. ${d}/detectOS.sh
+
+#echo "OS: $OS"
+#echo "DIST: $DIST"
+#echo "PSUEDONAME: $PSUEDONAME"
+#echo "REV: $REV"
+#echo "DistroBasedOn: $DistroBasedOn"
+#echo "KERNEL: $KERNEL"
+#echo "MACH: $MACH"
+#echo "SUB_REV=$SUB_REV
+#echo "========"
+
+###############################
+
+
+if [[ $DIST == "CentOS Linux" ]]  && [[ $SUB_REV  == "8" ]]
+  join_CentOS_8
+fi
+
+if [[ $DIST == "Ubuntu" ]]  && [[ $REV  == "18.04" ]]
+  then join_Ubuntu_18_04
+fi
+
