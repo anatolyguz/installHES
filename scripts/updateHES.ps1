871fdfa3214801c9e960c871028e49f5e736c758
anatolyguz <anatolyguz@gmail.com>
2021-05-27 23:44:58 +0300

    Update updateHES.ps1

--- scripts/updateHES.ps1
+++ scripts/updateHES.ps1
@@ -36,6 +36,19 @@ git pull
 git checkout $TAG
 
 
+if ($?) 
+{
+ echo "switch to branch $BRANCH"
+} 
+else 
+{
+    # ups.... 
+  echo 'error switchung to branch $BRANCH'
+  exit 1
+}
+
+
+
 #for testing  version only
 get-content HES.Web/HES.Web.csproj | select-string "<Version>"
 
@@ -47,14 +60,54 @@ cd $env:windir\System32\inetsrv
 
 #$env:windir\System32\inetsrv\appcmd.exe stop site /site.name:$HESSERVICE
 
-.\appcmd.exe  stop site /site.name:$HESSERVICE
 
-$BACKUP_HES_DIR = $HES_DIR + (Get-Date -Format "yyyy-mm-dd-HH-mm-ss") + ".Old"
+#.\appcmd.exe  stop site /site.name:$HESSERVICE
+#if ($?)
+#{
+#    echo "Site stopped"
+#}
+#
+#else
+#{
+#   echo 'Site not stopped!'
+#   exit 1
+#}
+
+#iisreset
+
+
+
+
+
+New-Item -Path $HES_DIR -Name "app_offline.htm" -ItemType "file"
+
+iisreset /stop
+
+
+
+$BACKUP_HES_DIR = $HES_DIR + "-" + (Get-Date -Format "yyyy-mm-dd-HH-mm-ss") + ".Old"
 
 
 write-host "BACKUP_HES_DIR = $BACKUP_HES_DIR"
 
 Rename-Item -Path $HES_DIR -NewName $BACKUP_HES_DIR
+
+if ($?)
+{
+    echo "Create of backup HES dir"
+}
+
+else
+{
+   echo 'Error Create of backup HES dir!'
+   exit 1
+}
+
+
+
+Remove-Item -Path $BACKUP_HES_DIR\app_offline.htm
+
+
 
 
 $MYSQL_HOME='C:\Program Files\MySQL\MySQL Server 8.0'
@@ -64,16 +117,63 @@ Set-Location "$MYSQL_HOME\bin"
 
 #& .\mysqldump.exe -u user -ppassword  db > C:\db.sql 
 
- .\mysqldump.exe -u root -p"$MYSQL_ROOT_PASSWORD"  --result-file=C:\db.sql  --databases $DATABASE_NAME
+# .\mysqldump.exe -u root -p"$MYSQL_ROOT_PASSWORD"  --result-file=C:\db.sql  --databases "$DATABASE_NAME"
 
 
+
+$tmpfie = New-TemporaryFile
+echo $tmpfie.FullName
+
+
+
+Add-Content -path $tmpfie.FullName @"
+[client]
+password = "$MYSQL_ROOT_PASSWORD"
+
+[mysqldump]
+user = root
+host = localhost
+"@
+
+
+
+
+#  .\mysqldump.exe  --defaults-extra-file=c:\123.txt -u root --result-file=$BACKUP_HES_DIR\$DATABASE_NAME.sql  "$DATABASE_NAME"
+
+
+
+echo   .\mysqldump.exe  --defaults-extra-file="$tmpfie".FullName -u root --result-file=$BACKUP_HES_DIR\$DATABASE_NAME.sql  "$DATABASE_NAME"
+  .\mysqldump.exe  --defaults-extra-file=$tmpfie -u root --result-file=$BACKUP_HES_DIR\$DATABASE_NAME.sql  "$DATABASE_NAME"
+
+
+
+
+
+if ($?)
+{
+    echo "create dump of database is correct"
+}
+
+else
+{
+   echo 'Failure create dump of database!'
+   exit 1
+}
+
+
+
+
+Remove-Item $tmpfie.FullName
 
 
 #.\mysqldump.exe --defaults-file=$temfile.FullName --result-file=C:\db.sql  --databases $DATABASE_NAME
 
 
-#write-host "temfile.FullName = $temfile.FullName"
-
-
-
 #cmd /c .\mysql.exe -N -s -r -u root -p"$ppp" -e 'show databases'
+
+
+
+cd $DESTINATION/HES.Web/
+
+dotnet publish -c $VERSION -v d -o $HES_DIR --runtime win-x64 HES.Web.csproj
+
